aa0861298e4d50f48c8c4b9fba48880ebc8731bf
liber1988 <92869703+liber1988@users.noreply.github.com>
2024-05-24 13:06:00 +0300

    Tiles models

---
+++ models/tiles-models.js
@@ -0,0 +1,13 @@
+export default {
+  soil: "soil",
+  grass: "grass",
+  wood: "wood",
+  stone: "stone",
+  cloud: "cloud",
+  leaves: "leaves",
+  wooder: "wooder",
+  leaveser: "leaveser",
+  gold: "gold",
+  diamond: "diamond",
+  sky: "sky",
+};
